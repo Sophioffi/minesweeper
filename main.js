5e61d54d6956adcb1051c0fdb6edde2424250a75
Sophie Loehr <sophie.c.loehr@gmail.com>
2016-04-02 23:11:00 +0200

    Add Cellreveal for 0 bomb cells, add hiding of bombs

--- main.js
+++ main.js
@@ -12,6 +12,19 @@ $(function() {
   var _state = _States.playing;
   var _gameSize = 10;
 
+  var revealCell = function(id, cell) {
+    if ($(cell).hasClass('cell-revealed')) { return; }
+    
+    $(cell).addClass('cell-revealed');
+    $(cell).text($(cell).attr('data-nr-bombs'));
+
+    if ($(cell).attr('data-nr-bombs') == 0) {
+      processNeighbours(id, function(neighbourId, neighbourCell) {
+        revealCell(neighbourId, neighbourCell);
+      });
+    }
+  }
+
   /*
   Left click:
     If the cell is a bomb, show a message ending the game.
@@ -20,10 +33,14 @@ $(function() {
   */
   var bindCells = function() {
     _cells.click(function(event) {
+      var id = _cells.index(this);
       if (event.which != 1) { return; }
       if ($(this).hasClass('cell-bomb')) {
-        alert('Game Over');
+        $('.cell-bomb').addClass('cell-bomb-visible');
         _state = _States.lost;
+        $('#message').text('Game Over!');
+      } else {
+        revealCell(id, this);
       }
     });
     _cells.bind('contextmenu', function() {
@@ -52,59 +69,76 @@ $(function() {
     }
   };
 
+  /*
+  Find cell with given id, then run a certain function (`thingToDo`)
+  for each of its neighbours, supplying that neighbour's id and cell.
+  */
+  var processNeighbours = function(id, thingToDo) {
+    var cell = _cells.eq(id);
+
+    var tmId = id - _gameSize;
+    var bmId = id + _gameSize;
+
+    /*
+    If we're on the left edge, set the left cell ids
+    to -1, so that we don't consider them.
+    Left edge cell: id multiple of game size
+    */
+    if (id % _gameSize == 0) {
+      var tlId = -1;
+      var mlId = -1;
+      var blId = -1;
+    } else {
+      var tlId = tmId - 1;
+      var mlId = id - 1;
+      var blId = bmId - 1;
+    }
+
+    /*
+    Same for the right edge.
+    Right edge cell: subtracting `gameSize - 1` makes it
+    a left edge cell.
+    */
+    if ((id - (_gameSize - 1)) % _gameSize == 0) {
+      var trId = -1;
+      var mrId = -1;
+      var brId = -1;
+    } else {
+      var trId = tmId + 1;
+      var mrId = id + 1;
+      var brId = bmId + 1;
+    }
+
+    var idsToCheck = [
+      tlId, tmId, trId,
+      mlId, mrId,
+      blId, bmId, brId
+    ];
+
+    for (idToCheck of idsToCheck) {
+      if (idToCheck < 0 || idToCheck > _gameSize * _gameSize) {
+        continue;
+      }
+      var cellToCheck = _cells.eq(idToCheck);
+      thingToDo(idToCheck, cellToCheck);
+    }
+  };
+
+  /*
+  For each cell, look at its neighbours, and count how many bombs there
+  are. Remember this in the cell.
+  */
   var distributeNumbers = function() {
     _cells.each(function(id, cell) {
-      var tmId = id - _gameSize;
-      var bmId = id + _gameSize;
-
-      /*
-      If we're on the left edge, set the left cell ids
-      to -1, so that we don't consider them.
-      Left edge cell: id multiple of game size
-      */
-      if (id % _gameSize == 0) {
-        var tlId = -1;
-        var mlId = -1;
-        var blId = -1;
-      } else {
-        var tlId = tmId - 1;
-        var mlId = id - 1;
-        var blId = bmId - 1;
-      }
-
-      /*
-      Same for the right edge.
-      Right edge cell: subtracting `gameSize - 1` makes it
-      a left edge cell.
-      */
-      if ((id - (_gameSize - 1)) % _gameSize == 0) {
-        var trId = -1;
-        var mrId = -1;
-        var brId = -1;
-      } else {
-        var trId = tmId + 1;
-        var mrId = id + 1;
-        var brId = bmId + 1;
-      }
-
-      var idsToCheck = [
-        tlId, tmId, trId,
-        mlId, mrId,
-        blId, bmId, brId
-      ];
-
       var nrBombs = 0;
-      for (idToCheck of idsToCheck) {
-        if (idToCheck < 0 || idToCheck > _gameSize * _gameSize) {
-          continue;
-        }
-        var cellToCheck = _cells.eq(idToCheck);
-        if (cellToCheck.hasClass('cell-bomb')) {
+      processNeighbours(id, function(neighbourId, neighbourCell) {
+        if (neighbourCell.hasClass('cell-bomb')) {
           nrBombs++;
         }
-      }
-
-      $(cell).text(nrBombs);
+      });
+      if (!$(cell).hasClass('cell-bomb')) {
+        $(cell).attr('data-nr-bombs', nrBombs)
+      };
     });
   }
 
