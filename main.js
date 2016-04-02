bc4c52e2ff0d95d8f6225e7af392d159383d1efd
Sophie Loehr <sophie.c.loehr@gmail.com>
2016-04-02 18:20:02 +0200

    Add States of game.

--- main.js
+++ main.js
@@ -1,31 +1,50 @@
 $(function() {
-  // Remember all the cells in the page.
-  var cells = $('.cell');
+  // All the cells in the page.
+  var _cells = $('.cell');
+
+  // The possible states of the game.
+  var _States = {};
+  _States.playing = 1;
+  _States.lost = 2;
+  _States.won = 3;
+
+  // The current state of the game.
+  var _state = _States.playing;
 
   /*
+  Left click:
+    If the cell is a bomb, show a message ending the game.
+    Otherwise, ...
   Right click: toggle `cell-flag` class.
   */
   var bindCells = function() {
-    cells.bind("contextmenu", function() {
+    _cells.click(function(event) {
+      if (event.which != 1) { return; }
+      if ($(this).hasClass('cell-bomb')) {
+        alert('Game Over');
+        _state = _States.lost;
+      }
+    });
+    _cells.bind('contextmenu', function() {
       $(this).toggleClass('cell-flag');
       return false;
     });
   };
 
   var clearCells = function() {
-    cells.removeClass('cell-bomb cell-flag');
+    _cells.removeClass('cell-bomb cell-flag');
   };
 
   /*
   Determine number of bombs, and randomly add them.
   */
   var distributeBombs = function() {
-    var nrCells = cells.length;
+    var nrCells = _cells.length;
     var nrBombs = Math.round(nrCells / 3);
     var nrBombsAdded = 0;
     while (nrBombsAdded < nrBombs) {
       var randomIndex = Math.round(Math.random() * nrCells);
-      var randomCell = cells.eq(randomIndex);
+      var randomCell = _cells.eq(randomIndex);
       if (!randomCell.hasClass('cell-bomb')) {
         randomCell.addClass('cell-bomb');
         nrBombsAdded++;
